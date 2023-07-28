9541952a8ba6d63661416d8efec7bcd1eb2c0973
Dom <domvit64@gmail.com>
2023-07-27 19:19:46 -0700

    Added input check class

--- Warhammer40kDamage/Form1.cs
+++ Warhammer40kDamage/Form1.cs
@@ -4,12 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-
-
 
 
 namespace Warhammer40kDamage
@@ -24,15 +21,31 @@ namespace Warhammer40kDamage
 
         private void runCalcButton_Click(object sender, EventArgs e)
         {
-                   
-            WeaponObject weapon = new WeaponObject(weaponNameTextBox.Text, weaponTypeComboBox.Text, int.Parse(strengthTextBox.Text), int.Parse(weaponSkillTextBox.Text),
-                                                   int.Parse(attacksTextBox.Text), int.Parse(apTextbox.Text), int.Parse(damageTextBox.Text));
+            try
+            {
 
-            DummySheetSetup dummySheets = new DummySheetSetup();
-            weapon.testCalc(dummySheets.Setup(enemySheetListBox.Text));
-   
+                InputCheck input = new InputCheck(weaponNameTextBox.Text, strengthTextBox.Text, weaponSkillTextBox.Text,
+                    attacksTextBox.Text, apTextbox.Text, damageTextBox.Text, enemySheetListBox.Text);
+
+                //WeaponObject weapon = new WeaponObject(weaponNameTextBox.Text, weaponTypeComboBox.Text, int.Parse(strengthTextBox.Text), int.Parse(weaponSkillTextBox.Text),
+                                                   //int.Parse(attacksTextBox.Text), int.Parse(apTextbox.Text), int.Parse(damageTextBox.Text));
+
+                //DummySheetSetup dummySheets = new DummySheetSetup();
+                //weapon.testCalc(dummySheets.Setup(enemySheetListBox.Text));
+
+            }
+
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.Message, "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            
 
         }
+
+        
 
         
     }
---
+++ Warhammer40kDamage/InputValidation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Warhammer40kDamage
+{
+     class InputCheck
+    {
+        public InputCheck(string weaponName, string strength, string weaponSkill, string attacks, string ap, string damage, string dummy) {
+
+            if(string.IsNullOrEmpty(weaponName))
+            {
+                throw new Exception("No name entered. Please enter a weapon name.");
+            }
+
+            if (string.IsNullOrEmpty(strength))
+            {
+                throw new Exception("No value entered for strength. Please enter a value.");
+            }
+        }
+        public bool WeaponNameCheck(string weaponName) {
+        
+            if(weaponName == null || weaponName == "")
+            {
+                MessageBox.Show("Please enter the name of the weapon used.", "No Weapon Name",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+        public bool StrengthCheck(int strength) {
+
+            Regex regex = new Regex("^[0-9]+$");
+
+            if(regex.Equals(strength))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Please enter a number for the strength value.", "Incorrect Strength Amount",
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+        public bool WeaponSkillCheck(int weaponSkill) {
+
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (regex.Equals(weaponSkill))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Please enter a number for the weapon skill value.", "Incorrect Weapon Skill Amount",
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+        public bool AttacksCheck(int attacks) {
+
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (regex.Equals(attacks))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Please enter a number for the attacks value.", "Incorrect Attack Amount",
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+        public bool APCheck(int ap) {
+
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (regex.Equals(ap))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Please enter a number for the ap value.", "Incorrect AP Amount",
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+        public bool DamageCheck(int damage) {
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (regex.Equals(damage))
+            {
+                return true;
+            }
+
+            MessageBox.Show("Please enter a number for the damage value.", "Incorrect Damage Amount",
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+        public bool DummyCheck(string dummy) {
+
+            return true;
+        }
+
+    }
+}
